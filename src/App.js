3e8cd6a4d00e49a59dbd9e8e7a45bb4afbe5571b
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-26 14:51:17 -0500

    resume data port to js file

--- src/App.js
+++ src/App.js
@@ -1,55 +1,31 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Portfolio from './Components/Portfolio';
+import React  from "react";
+import ReactGA from "react-ga";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portfolio";
+import { resumeData } from "./resumeData";
 
-export default function App () {
+const App = () => {
+  ReactGA.initialize("UA-208609134-1");
+  ReactGA.pageview(window.location.pathname);
 
-  constructor(props){
-    super(props);
-    this.state = {
-      resumeData: {}
-    };
-
-    ReactGA.initialize('UA-208609134-1');
-    ReactGA.pageview(window.location.pathname);
-
-  }
-
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-  }
-
-  
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <About data={this.state.resumeData.main}/>
-        <Resume data={this.state.resumeData.resume}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
-        <Footer data={this.state.resumeData.main}/>
-      </div>
-    );
-  
-}
-
+  return (
+    <div className="App">
+      {resumeData.map((data) => {
+        return (
+          <div>
+            <Header data={data.main} />
+            <About data={data.main} />
+            <Resume data={data.resume} />
+            <Portfolio data={data.portfolio} />
+            <Footer data={data.main} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,165 @@
+export const resumeData = [
+  {
+    main: {
+      name: "Hector Trejo Luna",
+      occupation: "Full-Stack Web Developer",
+      description:
+        "One of my passions in life is about discover, learn and practice new web technologies, I wake up everyday thinking of a new code challenge to resolve, and I produce music and digital art too",
+      image: "profilepic.jpg",
+      bio: "I like to code interesting software using high demanded technologies in the industry, I consider me as a Full Stack Web Developer, I had a background on WordPress, today I learn and practice from mastering basics (HTML, CSS, Javascript, Algorithms and Data Structures) and engage using ReactJS and NodeJS as prime Stack, I'm preparing for get the best job that kickstart my carrer as a sucessfull developer, sharp skills and learn something new everyday.",
+      contactmessage:
+        "I can help you in Frontend(React) and Backend(NodeJS) web development, contact me ASAP, for shure I'm on something",
+      email: "hectorsebastiantrejoluna@gmail.com",
+      phone: "+52 1 9985 56 6819",
+      address: {
+        street: "r 91 mz 80 52 pte",
+        city: "Cancun",
+        state: "Quintana Roo",
+        zip: "77516",
+      },
+      website: "http://hstrejoluna.com.mx",
+      resumedownload:
+        "https://drive.google.com/file/d/1ACR7Vt4S752CWKN7QYjYCVeH9qW6c-uB/view?usp=sharing",
+      social: [
+        {
+          name: "facebook",
+          url: "https://www.facebook.com/hstrejoluna",
+          className: "fa fa-facebook",
+        },
+
+        {
+          name: "linkedin",
+          url: "https://www.linkedin.com/in/hstrejoluna/",
+          className: "fa fa-linkedin",
+        },
+        {
+          name: "instagram",
+          url: "https://www.instagram.com/hstrejoluna/",
+          className: "fa fa-instagram",
+        },
+        {
+          name: "github",
+          url: "https://github.com/hstrejoluna/",
+          className: "fa fa-github",
+        },
+      ],
+    },
+    resume: {
+      education: [
+        {
+          school: "Universidad del Caribe",
+          degree: "Organizational Intelligence and Data Enginnering",
+          graduated: "Non-Graduated",
+        },
+        {
+          school: "CECyTE I Cancun",
+          degree: "Technician Support and Maintenance of Computer Equipment",
+        },
+      ],
+      work: [
+        {
+          company: "Kekhol Inmobiliaria",
+          title: "WordPress Web Developer and IT Assistant ",
+          years: "may 2020 – march 2021",
+          description:
+            "IT Assistant \n- Intranet Network Configuration, Windows Server, Email Server setting up.• Printers setup and support.• Ticket resolution (i.e. Problems with hardware, software ). WordPress Based Web Development:• Taking Requirements .• Hosting Administration (Billing, Support, Setting Up).• FTP Server Administrator.• MySQL Database Administrator, PhpMyAdmin.• FullStack Web Development  (php, javascript, html, css.) • ICustom Plugin Development, license administrator, Theme Customization. - Managment of analytics tools ( Google Analytics, Facebook Pixel, and more)",
+        },
+        {
+          company: "Grupo DEPI del Caribe",
+          title: "WordPress Web Developer and IT Assistant ",
+          years: "nov 2019 – april 2020",
+          description:
+            "IT Assistant \n- Intranet Network Configuration, Windows Server, Email Server setting up.• Printers setup and support.• Ticket resolution (i.e. Problems with hardware, software ). WordPress Based Web Development:• Taking Requirements .• Hosting Administration (Billing, Support, Setting Up).• FTP Server Administrator.• MySQL Database Administrator, PhpMyAdmin.• FullStack Web Development  (php, javascript, html, css.) • ICustom Plugin Development, license administrator, Theme Customization. - Managment of analytics tools ( Google Analytics, Facebook Pixel, and more)",
+        },
+      ],
+    },
+    portfolio: {
+      projects: [
+        {
+          title: "Kekhol Inmobiliaria",
+          category: "Install and Config theme and plugins, technical SEO",
+          image: "p4.png",
+          url: "https://www.kekhol.com",
+        },
+        {
+          title: "Krystal Beach Club",
+          category: "Install and Config theme and plugins, technical SEO",
+          image: "p5.png",
+          url: "http://krystalbeach.club",
+        },
+        {
+          title: "Arrivee Lagoon",
+          category: "Install and Config plugins, technical SEO, maintenance",
+          image: "p15.png",
+          url: "https://www.arriveebacalar.com/",
+        },
+        {
+          title: "College Support FR",
+          category: "Fixing strings translations",
+          image: "p2.png",
+          url: "http://collegesupportpt.org/fr/",
+        },
+        {
+          title: "ESL Digital",
+          category: "Moodle Installation and customization, maintenance",
+          image: "p13.png",
+          url: "http://www.esldigital.com/info/",
+        },
+        {
+          title: "The Future Company.ca",
+          category: "Theme install, custom CSS.",
+          image: "p1.png",
+          url: "http://thefuturecompany.ca",
+        },
+        {
+          title: "Digitraductor",
+          category: "Database feeding, cleaning json with python queries",
+          image: "p16.png",
+          url: "http://thefuturecompany.ca",
+        },
+        {
+          title: "Uptask",
+          category: "Database feeding, cleaning json with python queries",
+          image: "p20.png",
+          url: "https://www.linkedin.com/posts/hstrejoluna_javascript-nodejs-express-activity-6847009281351536640-9hKa",
+        },
+        {
+          title: "Hotel Gatsby",
+          category: "Discovering GatsbyJS and DatoCMS",
+          image: "p22.png",
+          url: "https://hotelgatsbyhstrejoluna.netlify.app/",
+        },
+        {
+          title: "Budget Control",
+          category: "Budget control, see spendings and remaining budget",
+          image: "p21.png",
+          url: "https://goofy-mccarthy-70ee2a.netlify.app/",
+        },
+        {
+          title: "Crypto Price",
+          category: "Fetch API and shows prices of crypto",
+          image: "p10.png",
+          url: "https://admiring-bassi-77534c.netlify.app",
+        },
+        {
+          title: "Nexter",
+          category: "Discovering the power of css grid",
+          image: "p11.png",
+          url: "https://affectionate-engelbart-c56758.netlify.app/",
+        },
+        {
+          title: "Trillo",
+          category: "Discovering the power of Flexbox",
+          image: "p12.png",
+          url: "https://wizardly-kare-619f6f.netlify.app/",
+        },
+        {
+          title: "Natours",
+          category: "Using SASS and basic CSS/HTML",
+          image: "p14.png",
+          url: "https://pensive-lichterman-42b193.netlify.app/",
+        },
+      ],
+    },
+  },
+];
