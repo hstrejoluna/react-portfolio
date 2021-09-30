35211afb260d66e004c97e500c6a57b41132d84c
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-30 16:29:21 -0500

    gifs!

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,30 +6,26 @@ const Portfolio = ({ data }) => {
   //State for modal
   const [modal, setModal] = useState(false);
   const Toggle = () => setModal(!modal);
-
   if (data) {
     var projects = data.projects.map(function (projects) {
       var projectImage = "images/portfolio/" + projects.image;
       return (
-        <>
-         
-          <div key={projects.title} className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
+        <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a onClick={() => Toggle()} title={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
                 </div>
-                <div className="link-icon">
-                  <i className="fa fa-link"></i>
-                </div>
-              </a>
-            </div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
+            </a>
           </div>
-        </>
+        </div>
       );
     });
   }
